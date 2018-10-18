188d9a84fba4bdf539cc27a96618b2bd7ee051ef
ernesto <ernesto@pigmalionstudios.com.ar>
2018-10-18 11:01:02 -0300

    Init repo

---
+++ main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const Menu = electron.Menu;
+let mainWindow = null
+
+
+app.on('window-all-closed',() => {
+      if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow();
+    mainWindow.loadURL(`file://${app.getAppPath()}/index.html`);
+    mainWindow.on('closed', () => { mainWindow = null; });
+});
